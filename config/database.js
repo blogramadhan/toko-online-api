f7406cd629c39c473ae522369ac6221cf87e7915
Kurnia Ramadhan <96585178+blogramadhan@users.noreply.github.com>
2025-06-07 15:59:14 +0700

    Perbarui konfigurasi database dengan menambahkan nilai default untuk username dan database.

--- config/database.js
+++ config/database.js
@@ -2,9 +2,9 @@ require('dotenv').config();
 
 module.exports = {
     development: {
-        username: process.env.DB_USER || '',
+        username: process.env.DB_USER || 'root',
         password: process.env.DB_PASSWORD || '',
-        database: process.env.DB_NAME || '',
+        database: process.env.DB_NAME || 'toko_online',
         host: process.env.DB_HOST || 'localhost',
         port: process.env.DB_PORT || 3306,
         dialect: 'mysql',
